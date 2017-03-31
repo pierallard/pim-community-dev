1a098ce1b0f19eed7eb1e70c6163e287eb56783c
pierallard <pierallard@gmail.com>
2017-03-31 16:54:20 +0200

    [PIM-6288] Refactor messenger.js

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/job/common/edit/upload-launch.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/job/common/edit/upload-launch.js
@@ -57,7 +57,7 @@ define(
                         router.redirect(response.redirectUrl);
                     }.bind(this))
                     .fail(function () {
-                        messenger.notificationFlashMessage('error', __('pim_enrich.form.job_instance.fail.launch'));
+                        messenger.notify('error', __('pim_enrich.form.job_instance.fail.launch'));
                     });
                 } else {
                     $.post(this.getUrl(), {method: 'POST'}).
@@ -65,7 +65,7 @@ define(
                             router.redirect(response.redirectUrl);
                         })
                         .fail(function () {
-                            messenger.notificationFlashMessage('error', __('pim_enrich.form.job_instance.fail.launch'));
+                            messenger.notify('error', __('pim_enrich.form.job_instance.fail.launch'));
                         });
                 }
 
