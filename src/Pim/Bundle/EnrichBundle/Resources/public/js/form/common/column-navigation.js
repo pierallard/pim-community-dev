985bba1b50ef3b63b29f7740568d7a395fd7bcde
pierallard <pierallard@gmail.com>
2017-04-13 18:04:35 +0200

    [PIM-6374] Create a column navigation tab

---
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/form/common/column-navigation.js
@@ -0,0 +1,58 @@
+'use strict';
+/**
+ * Display navigation links in column for the tab display
+ *
+ * @author    Pierre Allard <pierre.allard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+define(
+    [
+        'jquery',
+        'underscore',
+        'pim/form',
+        'text!pim/template/form/column-navigation'
+    ],
+    function ($, _, BaseForm, template) {
+        return BaseForm.extend({
+            tabs: [],
+
+            template: _.template(template),
+
+            events: {
+                'click .AknColumn-navigationLink': 'selectTab'
+            },
+
+            configure: function () {
+                this.tabs = [];
+
+                this.listenTo(this.getRoot(), 'column-tab:register', this.registerTab);
+
+                return BaseForm.prototype.configure.apply(this, arguments);
+            },
+
+            render: function () {
+                this.$el
+                    .empty()
+                    .html(this.template({
+                        tabs: this.tabs,
+                        currentTab: 'TODO'
+                    }));
+            },
+
+            registerTab: function (event) {
+                this.tabs.push({
+                    code: event.code,
+                    isVisible: event.isVisible,
+                    label: event.label
+                });
+
+                this.render();
+            },
+
+            selectTab: function (event) {
+                this.getRoot().trigger('column-tab:select-tab', event);
+            }
+        });
+    }
+);
