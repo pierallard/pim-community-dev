79982742ab176ff3500c8839d10499b56ceec452
Pierre Allard <pierallard@gmail.com>
2017-04-26 13:52:33 +0200

    [PIM-6374] Set PEF selectors for attribute groups

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/form/common/attributes.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/form/common/attributes.js
@@ -44,7 +44,7 @@ define(
     ) {
         return BaseForm.extend({
             template: _.template(formTemplate),
-            className: 'tabbable tabs-left object-attributes',
+            className: 'tabbable object-attributes',
             events: {
                 'click .remove-attribute': 'removeAttribute'
             },
