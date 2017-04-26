4ca2f17986748df4f61bfdd1a8cf175021ae60bb
Pierre Allard <pierallard@gmail.com>
2017-04-26 16:24:29 +0200

    [PIM-6374] Set PEF selectors for association type

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/associations.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/associations.js
@@ -11,6 +11,7 @@ define(
     [
         'jquery',
         'underscore',
+        'oro/translator',
         'backbone',
         'pim/form',
         'text!pim/template/product/tab/associations',
@@ -27,6 +28,7 @@ define(
     function (
         $,
         _,
+        __,
         Backbone,
         BaseForm,
         formTemplate,
@@ -62,8 +64,8 @@ define(
                             var params = {
                                 product: this.getFormData().meta.id
                             };
-                            var paramValue = this.datagrids.products.getParamValue(associationType);
-                            params[this.datagrids.products.paramName] = paramValue;
+                            params[this.datagrids.products.paramName] =
+                                this.datagrids.products.getParamValue(associationType);
                             params.dataLocale = UserContext.get('catalogLocale');
 
                             return params;
@@ -102,7 +104,7 @@ define(
                 this.trigger('tab:register', {
                     code: this.code,
                     isVisible: this.isVisible.bind(this),
-                    label: _.__('pim_enrich.form.product.tab.associations.title')
+                    label: __('pim_enrich.form.product.tab.associations.title')
                 });
 
                 _.each(this.datagrids, function (datagrid) {
@@ -146,7 +148,8 @@ define(
                             locale: UserContext.get('catalogLocale'),
                             associationTypes: associationTypes,
                             currentAssociationTarget: this.getCurrentAssociationTarget(),
-                            currentAssociationType: this.getCurrentAssociationType()
+                            currentAssociationType: this.getCurrentAssociationType(),
+                            label: __('pim_enrich.form.product.tab.associations.association_type_selector')
                         })
                     );
                     this.renderPanes();
@@ -233,10 +236,10 @@ define(
                 var associationType = event.currentTarget.dataset.associationType;
                 this.setCurrentAssociationType(associationType);
 
-                $(event.currentTarget)
-                    .addClass('active AknVerticalNavtab-item--active')
-                    .siblings('.active.AknVerticalNavtab-item--active')
-                    .removeClass('active AknVerticalNavtab-item--active');
+                this.$el.find('.current-association-type').html($(event.currentTarget).text());
+                this.$el.find('.association-type-selector .AknDropdown-menuLink--active')
+                    .removeClass('AknDropdown-menuLink--active');
+                $(event.currentTarget).find('.AknDropdown-menuLink ').addClass('AknDropdown-menuLink--active');
 
                 this.$('.AknTitleContainer.association-type[data-association-type="' + associationType + '"]')
                     .removeClass('AknTitleContainer--hidden')
