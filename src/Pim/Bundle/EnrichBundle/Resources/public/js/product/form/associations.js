3d67784f0cd57c7c6093196a6529e2bee936735a
Pierre Allard <pierallard@gmail.com>
2017-05-03 12:23:30 +0200

    [PIM-6374] Fix reviews

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/associations.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/product/form/associations.js
@@ -148,7 +148,11 @@ define(
                             locale: UserContext.get('catalogLocale'),
                             associationTypes: associationTypes,
                             currentAssociationTarget: this.getCurrentAssociationTarget(),
-                            currentAssociationType: this.getCurrentAssociationType(),
+                            currentAssociationTypeCode: this.getCurrentAssociationType(),
+                            currentAssociationType: _.findWhere(
+                                associationTypes,
+                                {code: this.getCurrentAssociationType()}
+                            ),
                             label: __('pim_enrich.form.product.tab.associations.association_type_selector')
                         })
                     );
