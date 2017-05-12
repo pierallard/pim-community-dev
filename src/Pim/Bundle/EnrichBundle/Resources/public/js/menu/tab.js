f5ddfcd8e447711dbd0a6a56c06e948f9bbd8e80
pierallard <pierallard@gmail.com>
2017-05-12 15:53:46 +0200

    [PIM-6290] Initialize new navigation

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/menu/tab.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/menu/tab.js
@@ -12,18 +12,22 @@ define(
         'underscore',
         'oro/translator',
         'pim/form',
+        'pim/router',
         'text!pim/template/menu/tab'
     ],
     function (
         _,
         __,
         BaseForm,
+        router,
         template
     ) {
         return BaseForm.extend({
-            tagName: 'li',
-            className: 'AknMainMenu-item',
+            className: 'AknHeader-menuItem',
             template: _.template(template),
+            events: {
+                'click': 'redirect'
+            },
 
             /**
              * {@inheritdoc}
@@ -38,15 +42,22 @@ define(
              * {@inheritdoc}
              */
             render: function () {
-                if (0 === _.keys(this.extensions).length) {
-                    return this;
-                }
-
                 this.$el.html(this.template({
-                    title: __(this.config.title)
+                    title: __(this.config.title),
+                    icon: '/bundles/pimui/images/' + this.config.icon,
+                    iconHover: '/bundles/pimui/images/' + this.config.iconHover
                 }));
 
                 BaseForm.prototype.render.apply(this, arguments);
+            },
+
+            /**
+             * Redirect the user to the config destination
+             */
+            redirect: function () {
+                if (undefined !== this.config.to) {
+                    router.redirectToRoute(this.config.to);
+                }
             }
         });
     });
