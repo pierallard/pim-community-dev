cc0583bae1f5d0bff78f426112147e49bf9e0947
Damien Carcel <damien.carcel@gmail.com>
2017-05-16 15:00:03 +0200

    TIP-706: Enhance locale switching in behat

--- features/Context/WebUser.php
+++ features/Context/WebUser.php
@@ -327,6 +327,7 @@ class WebUser extends RawMinkContext
     public function iSwitchTheLocaleTo($locale)
     {
         $this->getCurrentPage()->getElement('Main context selector')->switchLocale($locale);
+        $this->wait();
     }
 
     /**
