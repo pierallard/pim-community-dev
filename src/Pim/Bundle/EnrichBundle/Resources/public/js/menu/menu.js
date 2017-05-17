44e0b16bdcefd7168304105a8cd603dac10b44fb
Pierre Allard <pierallard@gmail.com>
2017-05-17 08:36:52 +0200

    [PIM-6290] Add breadcrumb mechanics

--- src/Pim/Bundle/EnrichBundle/Resources/public/js/menu/menu.js
+++ src/Pim/Bundle/EnrichBundle/Resources/public/js/menu/menu.js
@@ -6,16 +6,61 @@
  * @author    Julien Sanchez <julien@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */h
+ */
 define(
     [
         'underscore',
-        'pim/form'
+        'pim/form',
+        'oro/mediator'
     ],
     function (
         _,
-        BaseForm
+        BaseForm,
+        mediator
     ) {
         return BaseForm.extend({
+            current: null,
+
+            /**
+             * {@inheritdoc}
+             */
+            initialize: function (config) {
+                mediator.on('pim_menu:highlight', this.highlight, this);
+
+                return BaseForm.prototype.initialize.apply(this, arguments);
+            },
+
+            /**
+             * {@inheritdoc}
+             */
+            render: function () {
+                // TODO Temporary, to delete.
+                this.$el.empty();
+
+                BaseForm.prototype.render.apply(this, arguments);
+
+                this.$el.prepend('Current highlight: ' + _.result(_.first(this.current), 'label', 'none'));
+            },
+
+            /**
+             * Highlight tue current menu
+             */
+            highlight: function (event) {
+                var result = [];
+
+                _.each(this.extensions, function (extension) {
+                    if (extension.code === event.paths[0]) {
+                        result.push({
+                            code: extension.code,
+                            route: extension.getRoute(),
+                            label: extension.getLabel()
+                        });
+                    }
+                });
+
+                this.current = result;
+                this.render();
+                event.origin.setBreadcrumbs(result);
+            }
         });
     });
